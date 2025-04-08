2b9f3852f3c25b75aee2ac3fe6430043e1037505
Alex Kutzke <alexkutzke@gmail.com>
2025-04-08 18:05:40 -0300

    Atualiza mainactivity

--- app/src/main/java/com/example/aula05counter/MainActivity.kt
+++ app/src/main/java/com/example/aula05counter/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aula05counter.ui.Counter.Counter
+import com.example.aula05counter.ui.Counter.RGBCounterScreen
 import com.example.aula05counter.ui.theme.Aula05CounterTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +23,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Aula05CounterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Counter(modifier = Modifier.padding(innerPadding))
+                    Surface(modifier = Modifier.padding(innerPadding)){
+                        RGBCounterScreen()
+                    }
                 }
             }
         }
